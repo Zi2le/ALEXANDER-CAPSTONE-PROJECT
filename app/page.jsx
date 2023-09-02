deae72192fcb2b310993311fa1a4b3de6d92dc9a
XANDERIS1 <ziltheb88@gmail.com>
2023-09-02 17:51:27 +0100

    rando21

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Fservice from '../app/fservice/page'
+import Navbar from '../app/navbar/page'
+import Seventhcomponent from '../app/seventhcomponent/page'
+import Footer from '../app/footer/page'
 const riesen = () => {
   return (
    <section>
+    <Navbar/>
       <Fservice/>
+      <Seventhcomponent/>
+      <Footer/>
     </section>
     
  
